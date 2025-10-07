0a135d4ac9513f135b275a86575a5bc3ea511830
Gihaluf <gihaluf.bojar@gmail.com>
2025-10-07 09:12:34 +0300

    Pievienotas komandas izvades klasei

--- src/pakotne/Izvade.java
+++ src/pakotne/Izvade.java
@@ -1,5 +1,38 @@
 package pakotne;
 
-public class Izvade {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.swing.JOptionPane;
+
+public class Izvade {
+	// Gala vērtējumu izvadīšana
+	static void Izvada() {
+			for(int i=0; i<Ievade.studenti.length; i++) {	
+				for(int j=0; j<Ievade.kriteriji.length; j++) {
+					JOptionPane.showInputDialog(null, 
+							"Studenta "+Ievade.studenti[i]+" vērtējums par kritēriju "+Ievade.kriteriji[j]+" ir "+Ievade.kriterijaVertejums[i][j]+", kura svars ir "+Ievade.kriterijaSvars[j],
+							"Izvade",
+							JOptionPane.QUESTION_MESSAGE);
+					System.out.println();
+				}
+				JOptionPane.showInputDialog(null, 
+						"Semestra vērtējums ir "+GalvenaKlase.df.format(Ievade.semestraVertejums[i])+" balles",
+						"Izvade",
+						JOptionPane.QUESTION_MESSAGE);
+			}
+			GalvenaKlase.scan.close();
+	}
+	static void Ieraksta() {
+		try {
+			FileWriter fw = new FileWriter("nosaukums", false);
+			PrintWriter pw = new PrintWriter(fw);
+			
+			
+		}catch(IOException e) {
+			JOptionPane.showMessageDialog(null, "Nesaglabā failā", "Kļūda",
+					JOptionPane.WARNING_MESSAGE);
+		}
+	}
 }
